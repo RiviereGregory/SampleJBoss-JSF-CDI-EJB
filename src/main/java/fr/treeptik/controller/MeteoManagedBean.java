ee9ac53f1af7019c148033c17ecb2baed1fc8b4c
RIVIERE Gregory <gregory.riviere@laposte.net>
2013-09-11 12:28:20 +0200

    Ajout meteo icone

--- src/main/java/fr/treeptik/controller/MeteoManagedBean.java
+++ src/main/java/fr/treeptik/controller/MeteoManagedBean.java
@@ -21,12 +21,15 @@ public class MeteoManagedBean {
 
 	private String temperature;
 
+	private Short weatherId;
+
 	@PostConstruct
 	public void init() {
 		System.out.println("INIT");
 		WeatherReturn cityWeatherByZIP = soap.getCityWeatherByZIP("90001");
 		city = cityWeatherByZIP.getCity();
 		temperature = cityWeatherByZIP.getTemperature();
+		weatherId = cityWeatherByZIP.getWeatherID();
 		System.out.println("Meteo : " + city);
 		System.out.println("Meteo Temp : " + temperature);
 
@@ -37,6 +40,44 @@ public class MeteoManagedBean {
 		DecimalFormat df = new DecimalFormat("#######.00");
 
 		return df.format(tempC);
+
+	}
+
+	public String iconeMeteo() {
+		String imageMeteo = "";
+
+		switch (weatherId) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			imageMeteo = "/template-JSF-CDI-EJB/images/Sunny.png";
+			break;
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+			imageMeteo = "/template-JSF-CDI-EJB/images/Rain.png";
+			break;
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+			imageMeteo = "/template-JSF-CDI-EJB/images/Cloudy_With_Dizzle.png";
+			break;
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+			imageMeteo = "/template-JSF-CDI-EJB/images/Thunderstorms.png";
+			break;
+		}
+
+		return imageMeteo;
 
 	}
 
@@ -54,6 +95,14 @@ public class MeteoManagedBean {
 
 	public void setTemperature(String temperature) {
 		this.temperature = temperature;
+	}
+
+	public Short getWeatherId() {
+		return weatherId;
+	}
+
+	public void setWeatherId(Short weatherId) {
+		this.weatherId = weatherId;
 	}
 
 }
